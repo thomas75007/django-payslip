e65b76c7b9118627a54420b645ab470630c06f7a
thomas75007 <t.lobut@gmail.com>
2020-12-30 11:39:21 +0100

    Update models.py
    Update of the package

--- payslip/models.py
+++ payslip/models.py
@@ -2,7 +2,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils.timezone import localtime, now
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
